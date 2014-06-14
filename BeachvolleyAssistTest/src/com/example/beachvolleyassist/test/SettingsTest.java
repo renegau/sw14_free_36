d56e5dd3e4a5aac2444eb95647c8d0067f501933
renegau <rene@gau.at>
2014-06-14 11:29:27 +0200

    [MH,RG] Refactor SettingsTest

--- BeachvolleyAssistTest/src/com/example/beachvolleyassist/test/SettingsTest.java
+++ BeachvolleyAssistTest/src/com/example/beachvolleyassist/test/SettingsTest.java
@@ -1,17 +1,23 @@
 package com.example.beachvolleyassist.test;
 
+import junit.framework.Assert;
+
 import com.example.beachvolleyassist.SettingsActivity;
+import com.example.beachvolleyassist.Settings;
 import com.example.beachvolleyassist.R;
 import com.robotium.solo.Solo;
 
+import android.app.Activity;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class SettingsTest extends ActivityInstrumentationTestCase2<SettingsActivity> {
 	
 	private Solo mySolo;
+	private Settings mySettings;
 	
 	public SettingsTest() {
 		super(SettingsActivity.class);
@@ -20,6 +26,9 @@ public class SettingsTest extends ActivityInstrumentationTestCase2<SettingsActiv
 	protected void setUp() throws Exception {
 		super.setUp();
 		mySolo = new Solo(getInstrumentation(), getActivity());
+		
+		Activity settings_activity = this.getActivity();
+		mySettings = (Settings) settings_activity.getApplication();
 	}
 
 	protected void tearDown() throws Exception {
@@ -38,4 +47,39 @@ public class SettingsTest extends ActivityInstrumentationTestCase2<SettingsActiv
 		mySolo.getText("Do you really want to cancel the game settings?");
 	}
 	
+	public void testSetTeamRedPlayer1()
+	{	
+		String text_input = new String("Test TeamRedPlayer1"); 
+		EditText edit_text = (EditText) mySolo.getView(R.id.editTextTeamRedPlayer1);
+		mySolo.clearEditText(edit_text);
+		mySolo.enterText(edit_text, text_input);
+		Assert.assertTrue(mySettings.getTeamRedPlayer1().equals(text_input));
+	}
+	
+	public void testSetTeamRedPlayer2()
+	{	
+		String text_input = new String("Test TeamRedPlayer2"); 
+		EditText edit_text = (EditText) mySolo.getView(R.id.editTextTeamRedPlayer2);
+		mySolo.clearEditText(edit_text);
+		mySolo.enterText(edit_text, text_input);
+		Assert.assertTrue(mySettings.getTeamRedPlayer2().equals(text_input));
+	}
+	
+	public void testSetTeamBluePlayer1()
+	{	
+		String text_input = new String("Test TeamBluePlayer1"); 
+		EditText edit_text = (EditText) mySolo.getView(R.id.editTextTeamBluePlayer1);
+		mySolo.clearEditText(edit_text);
+		mySolo.enterText(edit_text, text_input);
+		Assert.assertTrue(mySettings.getTeamBluePlayer1().equals(text_input));
+	}
+	
+	public void testSetTeamBluePlayer2()
+	{	
+		String text_input = new String("Test TeamBluePlayer2"); 
+		EditText edit_text = (EditText) mySolo.getView(R.id.editTextTeamBluePlayer2);
+		mySolo.clearEditText(edit_text);
+		mySolo.enterText(edit_text, text_input);
+		Assert.assertTrue(mySettings.getTeamBluePlayer2().equals(text_input));
+	}
 }
