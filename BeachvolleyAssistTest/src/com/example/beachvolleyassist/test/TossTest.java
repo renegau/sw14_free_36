47f745c25c695c20b8915d6c10efef54ec33f900
renegau <rene@gau.at>
2014-06-14 14:04:13 +0200

    [MH,RG] Additional tests for the TossActivity

--- BeachvolleyAssistTest/src/com/example/beachvolleyassist/test/TossTest.java
+++ BeachvolleyAssistTest/src/com/example/beachvolleyassist/test/TossTest.java
@@ -1,22 +1,32 @@
 package com.example.beachvolleyassist.test;
 
+import junit.framework.Assert;
+
+import com.example.beachvolleyassist.Settings;
 import com.example.beachvolleyassist.TossActivity;
 import com.example.beachvolleyassist.R;
+import com.example.beachvolleyassist.Settings.Player;
+import com.example.beachvolleyassist.Settings.Team;
 import com.robotium.solo.Solo;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class TossTest extends ActivityInstrumentationTestCase2<TossActivity> {
 	
 	private Solo mySolo;
+	
+	private int _teamRedButtonColorValue;
+	private int _teamBlueButtonColorValue;
+	private Settings mySettings;
 	
 	public TossTest() {
 		super(TossActivity.class);
@@ -25,25 +35,20 @@ public class TossTest extends ActivityInstrumentationTestCase2<TossActivity> {
 	protected void setUp() throws Exception {
 		super.setUp();
 		mySolo = new Solo(getInstrumentation(), getActivity());
+		
+		Activity settings_activity = this.getActivity();
+		mySettings = (Settings) settings_activity.getApplication();
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 	
-	public void test_1_TestAfterRandom()
+	public void testAfterRandom()
 	{	
-		mySolo.sleep(5000);
+		waitForRandom();
 		
-		View teamRedButton = mySolo.getView(R.id.buttonTeamRed);
-		ColorDrawable colorDrawableRed = (ColorDrawable) teamRedButton.getBackground();
-        int teamRedButtonColorValue = colorDrawableRed.getColor();
-		
-        View teamBlueButton = mySolo.getView(R.id.buttonTeamBlue);
-		ColorDrawable colorDrawableBlue = (ColorDrawable) teamBlueButton.getBackground();
-        int teamBlueButtonColorValue = colorDrawableBlue.getColor();
-		
-        // public static final int BLUE
+		// public static final int BLUE
         // Constant Value: -16776961 (0xff0000ff)
         //
         // public static final int RED
@@ -53,14 +58,398 @@ public class TossTest extends ActivityInstrumentationTestCase2<TossActivity> {
         // Constant Value: -1 (0xffffffff)
 
         assertTrue(
-        		( teamRedButtonColorValue == Color.RED && teamBlueButtonColorValue == Color.WHITE ) ||
-        		( teamRedButtonColorValue == Color.WHITE && teamBlueButtonColorValue == Color.BLUE) );
-		
+        		( _teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE ) ||
+        		( _teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE) );
 	}
 	
-	public void test_2_step2()
+	public void testTextStep2AndStep3()
 	{	
-	    
+		waitForRandom();
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+        	mySolo.getView(R.id.textViewStep2);
+    		mySolo.getText("Step 2/4: Winner of the toss TEAM RED is choosing?");
+    		
+    		mySolo.getView(R.id.textViewStep3);
+    		mySolo.getText("Step 3/4: Looser of the toss TEAM BLUE is choosing?");
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	mySolo.getView(R.id.textViewStep2);
+    		mySolo.getText("Step 2/4: Winner of the toss TEAM BLUE is choosing?");
+    		
+    		mySolo.getView(R.id.textViewStep3);
+    		mySolo.getText("Step 3/4: Looser of the toss TEAM RED is choosing?");
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+        
+	}
+	
+	public void testStep2ChooseService()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertFalse(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+        
+	}
+	
+	public void testStep2ChooseReturn()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Return));
+		mySolo.sleep(1000);
+		
+		assertFalse(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertFalse(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep2ChooseLeftSide()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Leftside));
+		mySolo.sleep(1000);
+		
+		assertFalse(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertTrue(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));
+        	assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep2ChooseLeftRight()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Rightside));
+		mySolo.sleep(1000);
+		
+		assertFalse(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertTrue(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));
+        	assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep3ChooseService()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Rightside));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Service));
+		mySolo.sleep(1000);
+		
+		assertFalse(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertTrue(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));
+		    
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));
+        	assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+        	
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep3ChooseReturn()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Rightside));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Return));
+		mySolo.sleep(1000);
+		
+		assertFalse(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertFalse(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));
+		    
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));
+        	assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+        	
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep3ChooseLeftside()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Leftside));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertFalse(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+		    
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        	
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));        	
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep3ChooseRightside()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Rightside));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonStep2Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Leftside).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep2Rightside).isEnabled());
+		
+		assertFalse(mySolo.getView(R.id.buttonStep3Service).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Return).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonStep3Leftside).isEnabled());
+		assertTrue(mySolo.getView(R.id.buttonStep3Rightside).isEnabled());
+		
+		// Team RED wins the toss
+        if (_teamRedButtonColorValue == Color.RED && _teamBlueButtonColorValue == Color.WHITE)
+        {
+		    assertTrue(mySettings.getFirstServiceTeam().equals(Team.RED));
+		    
+		    assertTrue(mySettings.getBeginningSideRight().equals(Team.BLUE));
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.RED));
+        }
+        // Team BLUE wins the toss
+        else if (_teamRedButtonColorValue == Color.WHITE && _teamBlueButtonColorValue == Color.BLUE)
+        {
+        	assertTrue(mySettings.getFirstServiceTeam().equals(Team.BLUE));
+        	
+        	assertTrue(mySettings.getBeginningSideRight().equals(Team.RED));
+		    assertTrue(mySettings.getBeginningSideLeft().equals(Team.BLUE));        	
+        }     
+        else
+        {
+        	assertFalse(true);
+        }
+	}
+	
+	public void testStep4ChoosePlayerRed1()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Rightside));
+		mySolo.sleep(1000);
+		
+		mySolo.clickOnView(mySolo.getView(R.id.buttonPlayerRed1));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonPlayerRed1).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonPlayerRed2).isEnabled());
+		
+		assertTrue(mySettings.getFirstServicePlayerRed().equals(Player.ONE));
+	}
+	
+	public void testStep4ChoosePlayerRed2()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Rightside));
+		mySolo.sleep(1000);
+		
+		mySolo.clickOnView(mySolo.getView(R.id.buttonPlayerRed2));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonPlayerRed2).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonPlayerRed1).isEnabled());
+		
+		assertTrue(mySettings.getFirstServicePlayerRed().equals(Player.TWO));
+	}
+	
+	public void testStep4ChoosePlayerBlue1()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Rightside));
+		mySolo.sleep(1000);
+		
+		mySolo.clickOnView(mySolo.getView(R.id.buttonPlayerBlue1));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonPlayerBlue1).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonPlayerBlue2).isEnabled());
+		
+		assertTrue(mySettings.getFirstServicePlayerBlue().equals(Player.ONE));
+	}
+	
+	public void testStep4ChoosePlayerBlue2()
+	{	
+		waitForRandom();
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep2Service));
+		mySolo.sleep(1000);
+		mySolo.clickOnView(mySolo.getView(R.id.buttonStep3Rightside));
+		mySolo.sleep(1000);
+		
+		mySolo.clickOnView(mySolo.getView(R.id.buttonPlayerBlue2));
+		mySolo.sleep(1000);
+		
+		assertTrue(mySolo.getView(R.id.buttonPlayerBlue2).isEnabled());
+		assertFalse(mySolo.getView(R.id.buttonPlayerBlue1).isEnabled());
+		
+		assertTrue(mySettings.getFirstServicePlayerBlue().equals(Player.TWO));
 	}
 	
 	public void testActivityTitle()
@@ -75,4 +464,17 @@ public class TossTest extends ActivityInstrumentationTestCase2<TossActivity> {
 		mySolo.getText("Do you really want to cancel the game settings?");
 	}
 
+
+	private void waitForRandom()
+	{
+		mySolo.sleep(5000);
+		
+		View teamRedButton = mySolo.getView(R.id.buttonTeamRed);
+		ColorDrawable colorDrawableRed = (ColorDrawable) teamRedButton.getBackground();
+		_teamRedButtonColorValue = colorDrawableRed.getColor();
+		
+		View teamBlueButton = mySolo.getView(R.id.buttonTeamBlue);
+		ColorDrawable colorDrawableBlue = (ColorDrawable) teamBlueButton.getBackground();
+		_teamBlueButtonColorValue = colorDrawableBlue.getColor();
+	}
 }
